fb082a3524a2537c1c6c53b97f0541954de92083
Divya-R-M <divyarm275@gmail.com>
2020-11-25 21:04:19 +0530

    Added doc on Project Domain

--- src/main/java/com/cg/cap_project/ppmtoolapi/domain/Project.java
+++ src/main/java/com/cg/cap_project/ppmtoolapi/domain/Project.java
@@ -1,5 +1,10 @@
 package com.cg.cap_project.ppmtoolapi.domain;
 
+/*
+ * Project domain is used as data traveller object.
+ * All Project validation will be performed here.
+ * @author Divya
+ */
 public class Project {
 
 }
