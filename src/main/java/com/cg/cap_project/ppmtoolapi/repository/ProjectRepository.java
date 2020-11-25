123f8392a5b7c6134c3e3c7e9735c679d89ff5da
Divya-R-M <divyarm275@gmail.com>
2020-11-25 23:54:29 +0530

    Project Domain updated and Repository created

---
+++ src/main/java/com/cg/cap_project/ppmtoolapi/repository/ProjectRepository.java
@@ -0,0 +1,13 @@
+package com.cg.cap_project.ppmtoolapi.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cg.cap_project.ppmtoolapi.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+
+	//TODO : no need to add CRUD methods, Methods can be customized.
+	
+}
